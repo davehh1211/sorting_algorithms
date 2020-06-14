c60a5bee409e10c6a8393a914144fd01f22438f6
davehh1211 <davehh1211@gmail.com>
2020-06-14 18:17:42 +0000

    insertion v1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - explains the way how to sort
+ * and insert in linked list
+ * @list: the linked list to be sorted
+ * Return: nothing
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *tmp;
+
+	tmp = (*list)->next;
+
+	while (tmp)
+	{
+		while (tmp->prev && tmp->prev->n > tmp->n)
+		{
+			if (tmp->next)
+				tmp->next->prev = tmp->prev;
+			tmp->prev->next = tmp->next;
+			tmp->next = tmp->prev;
+			tmp->prev = tmp->prev->prev;
+			tmp->next->prev = tmp;
+
+			if (tmp->prev == NULL)
+				*list = tmp;
+			else
+				tmp->prev->next = tmp;
+			print_list(*list);
+		}
+		tmp = tmp->next;
+	}
+}
